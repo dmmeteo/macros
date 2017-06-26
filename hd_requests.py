b6094dd0a1ff4119b024dac186e318d185f3f6e3
Dmitry Sichkar <dmmeteo@gameil.com>
2017-06-26 11:39:17 +0300

    fixed post(not async)

--- hd_requests.py
+++ hd_requests.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import requests
+import grequests
 from bs4 import BeautifulSoup
 from getpass import getpass
-
 
 USER, PASW = raw_input('Username:'), getpass()
 SPAM = ['example@spam.com']
@@ -20,31 +20,32 @@ with requests.session() as s:
     
     # Aggregated spam urls & mails
     spam_list = [{
-                    'mail': e.get_text(),
-                    'url': SECONDARY_URL % ''.join([e for e in e.parent['onclick'] if e.isdigit()]),
-                } for e in BeautifulSoup(resp.text, 'lxml').select('td') if e.get_text() in SPAM]
-
+        'mail': e.get_text(),
+        'url': SECONDARY_URL % ''.join([e for e in e.parent['onclick'] if e.isdigit()]),
+    } for e in BeautifulSoup(resp.text, 'lxml').select('td') if e.get_text() in SPAM]
+    
     print('Spam mails: %s' % len(spam_list))
 
+    count = 0
     if spam_list:
-        # Create data from spamHTML inputs
-        prep = s.get(spam_list[-1]['url'])
-        data_dict = {f.get('name'): f.get('value') for f in BeautifulSoup(prep.text, 'lxml').select('input')
-                     if f.get('name') and f.get('value')}
-    
-        counter = 0
         for t in spam_list:
+            # Create data from spamHTML inputs
+            prep = s.get(t['url'])
+            data_dict = {f.get('name'): f.get('value') for f in BeautifulSoup(prep.text, 'lxml').select('input')
+                         if f.get('name') and f.get('value')}
+        
             # Do something with data
             data_dict.update({
-                'suerce': 'email',
-                'email': t['mail'],
+                'source': 'email',
                 'closed': 'on',
-                'submit': u'Сохранить'
+                'department': 'd_tehnicheskaya-podderzhka',
+                'priority': 'normal'
             })
-            r = s.post(t['url'], data=data_dict, headers=dict(Referer=SECONDARY_URL)) # TODO not working - need fix
-            print(r.content)
-            
-            counter += 1
-            print('kill %s' % counter)
-    else:
-        print('fin!')
+            # r = grequests.post(t['url'], data=data_dict, cookies=prep.cookies, headers=dict(Referer=SECONDARY_URL)) # TODO not working async
+            r = s.post(t['url'], data=data_dict, cookies=prep.cookies, headers=dict(Referer=t['url']))
+        
+            if count is not 0 and count % 10 == 0:
+                print('ready %s reuqests' % count)
+            count += 1
+
+    print('fin!')
