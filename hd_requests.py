28e4df8a2516064d928ae1636cf4d4bf0018d3d6
Dmitry Sichkar <dmmeteo@gameil.com>
2017-06-30 14:14:09 +0300

    fixed AsyncRequests

--- hd_requests.py
+++ hd_requests.py
@@ -1,13 +1,25 @@
 # -*- coding: utf-8 -*-
+import sys
 import requests
 import grequests
 from bs4 import BeautifulSoup
 from getpass import getpass
 
+
+# check if using python 3
+print('python', sys.version)
+if sys.version_info[0] == 3:
+    xrange = range
+    raw_input = input
+
+# Main vars
 USER, PASW = raw_input('Username:'), getpass()
 SPAM = ['example@spam.com']
 BASE_URL = 'https://example.com/'
 SECONDARY_URL = 'https://example.com/something/%s/edit/'
+rs = []  # AsyncRequests list
+count = len(rs)  # AsyncRequests counter
+
 
 with requests.session() as s:
     # Retrieve the CSRF token first
@@ -21,31 +33,41 @@ with requests.session() as s:
     # Aggregated spam urls & mails
     spam_list = [{
         'mail': e.get_text(),
+        'subject': e.parent.select('td[title]')[0].get_text(),
         'url': SECONDARY_URL % ''.join([e for e in e.parent['onclick'] if e.isdigit()]),
     } for e in BeautifulSoup(resp.text, 'lxml').select('td') if e.get_text() in SPAM]
     
     print('Spam mails: %s' % len(spam_list))
-
-    count = 0
+    
     if spam_list:
+        # Create prep_data from spamHTML inputs
+        # TODO prep_data not only by inputs,
+        prep = s.get(spam_list[0]['url'])
+        prep_data = {f.get('name'): f.get('value') for f in BeautifulSoup(prep.text, 'lxml').select('input')
+                     if f.get('name') and f.get('value')}
+        
+        # Do something with data
+        prep_data.update({
+            'priority': 'normal'
+        })
+        
         for t in spam_list:
-            # Create data from spamHTML inputs
-            prep = s.get(t['url'])
-            data_dict = {f.get('name'): f.get('value') for f in BeautifulSoup(prep.text, 'lxml').select('input')
-                         if f.get('name') and f.get('value')}
-        
             # Do something with data
-            data_dict.update({
-                'source': 'email',
-                'closed': 'on',
-                'department': 'd_tehnicheskaya-podderzhka',
-                'priority': 'normal'
+            prep_data.update({
+                'email': t['mail'],
+                'subject': t['subject'],
             })
-            # r = grequests.post(t['url'], data=data_dict, cookies=prep.cookies, headers=dict(Referer=SECONDARY_URL)) # TODO not working async
-            r = s.post(t['url'], data=data_dict, cookies=prep.cookies, headers=dict(Referer=t['url']))
-        
-            if count is not 0 and count % 10 == 0:
-                print('ready %s reuqests' % count)
-            count += 1
 
+            # Aggregated AsyncRequest
+            rs.append(
+                grequests.post(t['url'], data=prep_data, session=s, headers=dict(Referer=t['url']))
+            )
+            
+            # Print ready requests
+            if count % 10 == 0:
+                print('ready %s reuqests' % count)
+
+        # Send them all at the same time
+        grequests.map(rs)
+    
     print('fin!')
